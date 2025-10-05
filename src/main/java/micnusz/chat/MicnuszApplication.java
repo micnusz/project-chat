980a6a63304e357fb143b90ac7943858f5c9e8c9
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-10-05 14:20:35 +0200

    Initial commit

---
+++ src/main/java/micnusz/chat/MicnuszApplication.java
@@ -0,0 +1,13 @@
+package micnusz.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicnuszApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicnuszApplication.class, args);
+	}
+
+}
